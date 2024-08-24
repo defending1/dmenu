807990fbb2bd53d1860cb4a7af470af1f93cf7e5
Alberto Defendi <albedef@protonmail.com>
2024-08-24 20:42:57 +0200

    Add lineheight patch

--- config.def.h
+++ config.def.h
@@ -15,7 +15,11 @@ static const char *colors[SchemeLast][2] = {
     [SchemeOut] = {"#000000", "#00ffff"},
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+
 static unsigned int lines = 0;
+/* -h option; minimum height of a menu line */
+static unsigned int lineheight = 0;
+static unsigned int min_lineheight = 8;
 
 /*
  * Characters not considered part of a word while deleting words
